d9be565184926fa7c8c8827bd30816c11dc98d9d
Mateusz <100945821+Mateusz022@users.noreply.github.com>
2022-03-31 22:32:02 +0200

    Add files via upload

---
+++ NWD .cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int m, n, roznica;
+            Console.WriteLine("Podaj wartośc m:");
+            m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj wartośc n");
+            n = Convert.ToInt32(Console.ReadLine()); 
+            do
+            {
+                if (m > n)
+                {
+                    roznica = m - n;
+                    m = roznica; 
+                }
+                else
+                {
+                    roznica = n-m;
+                    if (n > m)
+                    {
+                        n = roznica;
+                    }
+                }
+            }while(roznica!= 0);
+            Console.WriteLine("Największym wspólnym dzielnikiem jest wartośc zmiennej " + n);
+            Console.ReadKey();
+        }
+    }
+}
